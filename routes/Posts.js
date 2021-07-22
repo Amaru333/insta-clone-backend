be8a377f41728513b5db51fc206b82c7a4f67981
Amaru333 <amrutesharun0599@gmail.com>
2021-07-22 22:28:04 +0530

    Route to get all post through server

--- routes/Posts.js
+++ routes/Posts.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const router = express.Router();
 
+const { validateToken } = require("../middleware/AuthMiddleware");
+
 const { Posts } = require("../models");
 
 router.get("/", async (req, res) => {
@@ -8,8 +10,10 @@ router.get("/", async (req, res) => {
   res.json(listOfPosts);
 });
 
-router.post("/", async (req, res) => {
+router.post("/", validateToken, async (req, res) => {
   const post = req.body;
+  post.username = req.user.username;
+  post.UserId = req.user.id;
   await Posts.create(post);
   res.json(post);
 });
